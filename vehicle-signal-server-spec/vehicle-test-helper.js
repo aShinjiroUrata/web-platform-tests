95bfc12cd8fab19f055653f9aaa0ff1d97ddf099
aShinjiroUrata <shinjiro.urata@access-company.com>
2017-02-16 22:10:03 +0900

    add tests

--- vehicle-signal-server-spec/vehicle-test-helper.js
+++ vehicle-signal-server-spec/vehicle-test-helper.js
@@ -1,14 +1,71 @@
 //var VSSS_HOST = "xx.xx.xx.xx";
-//var VSSS_HOST = "127.0.0.1";
-var VISS_HOST = "10.5.162.71";
+var VISS_HOST = "10.5.162.79";
 var VISS_PORT = "3000";
+var VISS_SUBPROTO = "wvss1.0";
+
+//most tests uses this as URL to VISS server
 var VISS_URL = "ws://"+VISS_HOST+":"+VISS_PORT
 
-var TIME_FINISH_WAIT = 1000; // wait time to let human see result message
-
+var TIME_FINISH_WAIT = 500; // wait time to let human see result message
 var TIME_OUT_TIME = 3000;    // time to forcefully terminate the test
 
 // === get helper ===
+function isAuthorizeSuccessResponse( _reqId, _inJson) {
+  // TODO: better to check with Json schema
+  if (
+      _inJson.action === "authorize" &&
+      _inJson.requestId === _reqId &&
+      _inJson.TTL &&                //'TTL' exists
+      _inJson.error === undefined)  //'error' not exists
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isAuthorizeErrorResponse( _reqId, _inJson) {
+  // TODO: better to check with Json schema
+  if (
+      _inJson.action === "authorize" &&
+      _inJson.requestId === _reqId &&
+      _inJson.TTL === undefined &&  //'TTL' not exists
+      _inJson.error)                //'error' exists
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isVssSuccessResponse( _reqId, _inJson) {
+  // TODO: better to check with Json schema
+  if (
+      _inJson.action === "getVSS" &&
+      _inJson.requestId === _reqId &&
+      _inJson.vss &&                //'TTL' exists
+      _inJson.error === undefined)  //'error' not exists
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isVssErrorResponse( _reqId, _inJson) {
+  // TODO: better to check with Json schema
+  if (
+      _inJson.action === "authorize" &&
+      _inJson.requestId === _reqId &&
+      _inJson.vss === undefined &&  //'vss' not exists
+      _inJson.error)                //'error' exists
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
 function isGetSuccessResponse( _reqId, _inJson) {
   // TODO: better to check with Json schema
   // getSuccessResponse has action?
@@ -67,16 +124,52 @@ function isSubscribeErrorResponse( _reqId, _inJson) {
   }
 }
 
+function isSubscriptionNotification( _subId, _inJson) {
+  // TODO: better to check with Json schema
+  if (_inJson.subscriptionId && //'subscriptionId' just exists
+      _inJson.timestamp &&      //'timestamp' exists
+      _inJson.value)            //'value' exists
+  {
+    if (_subId === "" || _subId === _inJson.subscriptionId) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+function isSubscriptionNotificationError( _subId, _inJson) {
+  // TODO: better to check with Json schema
+  if (_inJson.subscriptionId &&
+      _inJson.timestamp &&      //'timestamp' exists
+      _inJson.error)            //'error' exists
+  {
+    // if _subId is empty string, don't check subId matching
+    if (_subId === "" || _subId === _inJson.subscriptionId) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
 // === unsubscribe helper ===
 function isUnsubscribeSuccessResponse( _reqId, _subId, _inJson) {
   // TODO: better to check with Json schema
-  // getSuccessResponse has action?
   if (_inJson.action === "unsubscribe" &&
       _inJson.requestId === _reqId &&
-      _inJson.subscriptionId === _subId &&
+      _inJson.subscriptionId &&
+      _inJson.error === undefined &&
       _inJson.timestamp)      //'timestamp' exists
   {
-    return true;
+    if (_subId === "" || _subId === _inJson.subscriptionId) {
+      return true;
+    } else {
+      return false;
+    }
   } else {
     return false;
   }
@@ -85,11 +178,26 @@ function isUnsubscribeErrorResponse( _reqId, _subId, _inJson) {
   // TODO: better to check with Json schema
   if (_inJson.action === "unsubscribe" &&
       _inJson.requestId === _reqId &&
-      _inJson.subscriptionId === _subId &&
+      _inJson.subscriptionId &&
       _inJson.timestamp &&      //'timestamp' exists
-      _inJson.error &&          //'error' exists
-      _inJson.error.number &&   //'error.number' exists
-      _inJson.error.reason)     //'error.reason' exists
+      _inJson.error)          //'error' exists
+  {
+    if (_subId === "" || _subId === _inJson.subscriptionId){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+function isUnsubscribeAllSuccessResponse( _reqId, _inJson) {
+  // TODO: better to check with Json schema
+  if (_inJson.action === "unsubscribe" &&
+      _inJson.requestId === _reqId &&
+      _inJson.subscriptionId === null &&
+      _inJson.error === undefined &&
+      _inJson.timestamp)      //'timestamp' exists
   {
     return true;
   } else {
@@ -147,5 +255,32 @@ function addLogFailure(_msg) {
         + "FAILURE : " + _msg
         + '</div>';
   document.getElementById('log').innerHTML = msg;
+}
+
+function helper_terminate_normal( _msg ) {
+  addLogMessage( _msg );
+  t.step_timeout(function() {
+    //assert_true(false, _msg);
+    vehicle.close();
+    t.done();
+  }, TIME_FINISH_WAIT);
+}
+function helper_terminate_success( _msg ) {
+//function helper_assert_success( _msg ) {
+  addLogSuccess( _msg );
+  t.step_timeout(function() {
+    assert_true(true, _msg);
+    vehicle.close();
+    t.done();
+  }, TIME_FINISH_WAIT);
+}
+function helper_terminate_failure( _msg ) {
+//function helper_assert_failure( _msg ) {
+  addLogFailure( _msg );
+  t.step_timeout(function() {
+    assert_throws(null, function(){}, _msg);
+    vehicle.close();
+    t.done();
+  }, TIME_FINISH_WAIT);
 }
 
